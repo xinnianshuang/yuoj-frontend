2996021281813caa93783f00cc6c1add56a034b0
main <2512325502@qq.com>
2024-08-02 03:01:36 +0800

    first Commit

---
+++ src/views/ExampleView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="exampleView">
+    <CodeEditor :value="mdValue" :handle-change="onMdChange" />
+    <MdEditor :value="codeValue" :handle-change="onCodeChange" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import MdEditor from "@/components/MdEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue";
+
+const codeValue = ref();
+const mdValue = ref();
+const onCodeChange = (v: string) => {
+  codeValue.value = v;
+  console.log(v);
+};
+const onMdChange = (v: string) => {
+  mdValue.value = v;
+  console.log(v);
+};
+</script>
